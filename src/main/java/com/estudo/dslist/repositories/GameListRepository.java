77f89dcc1592add305fe708c3456a3d8898db9f8
GabryelSantAna <byels.2002@gmail.com>
2023-11-16 19:54:58 -0300

    GEt lits

---
+++ src/main/java/com/estudo/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.estudo.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudo.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList,Long>{
+
+}
